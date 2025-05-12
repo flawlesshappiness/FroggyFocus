7667b38a3e49439c8ede05c716a853c5a8a3cc97
flawlesshappiness <bkk@arabesque.dk>
2025-05-12 19:44:34 +0200

    Added target values

--- froggyfocus/Views/MainMenuView/MainMenuView.cs
+++ froggyfocus/Views/MainMenuView/MainMenuView.cs
@@ -42,6 +42,8 @@ public partial class MainMenuView : View
         base.OnShow();
         MouseVisibility.Show(nameof(MainMenuView));
 
+        GameView.Instance.Hide();
+
         Overlay.Show();
         Overlay.Modulate = Overlay.Modulate.SetA(1);
         AnimationPlayer.Play("hide_overlay");
