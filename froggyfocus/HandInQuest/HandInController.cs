ca7bd217b56786b7982130aa289cde24034775f9
flawlesshappiness <bkk@arabesque.dk>
2025-07-08 10:01:36 +0200

    Added HandIn debug reset

--- froggyfocus/HandInQuest/HandInController.cs
+++ froggyfocus/HandInQuest/HandInController.cs
@@ -8,6 +8,36 @@ public partial class HandInController : ResourceController<HandInCollection, Han
 
     public event Action<string> OnHandInClaimed;
 
+    public override void _Ready()
+    {
+        base._Ready();
+        RegisterDebugActions();
+    }
+
+    private void RegisterDebugActions()
+    {
+        var category = "HAND IN";
+
+        Debug.RegisterAction(new DebugAction
+        {
+            Category = category,
+            Text = "Reset all",
+            Action = ResetAll
+        });
+
+        void ResetAll(DebugView v)
+        {
+            v.Close();
+
+            foreach (var data in Data.Game.HandIns)
+            {
+                data.DateTimeNext = GameTime.GetCurrentDateTimeString();
+            }
+
+            Data.Game.Save();
+        }
+    }
+
     public HandInInfo GetInfo(string id)
     {
         return Collection.Resources.FirstOrDefault(x => x.Id == id);
