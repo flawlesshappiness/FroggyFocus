ebdf70412948097f18ef1f540067472a6f60aacf
flawlesshappiness <bkk@arabesque.dk>
2025-07-08 08:15:13 +0200

    Added MushroomGuy npc

--- froggyfocus/Prefabs/NPC/MushroomNPC/MushroomNpc.cs
+++ froggyfocus/Prefabs/NPC/MushroomNPC/MushroomNpc.cs
@@ -1,3 +1,4 @@
+using FlawLizArt.Animation.StateMachine;
 using Godot;
 
 public partial class MushroomNpc : Area3D, IInteractable
@@ -5,17 +6,47 @@ public partial class MushroomNpc : Area3D, IInteractable
     [Export]
     public HandInInfo HandInInfo;
 
+    [Export]
+    public AnimationStateMachine Animation;
+
+    private readonly BoolParameter param_dialogue = new BoolParameter("dialogue", false);
+
     public override void _Ready()
     {
         base._Ready();
         HandIn.InitializeData(HandInInfo);
         HandInController.Instance.OnHandInClaimed += HandInClaimed;
+
+        DialogueController.Instance.OnNodeEnded += DialogueEnded;
+        DialogueController.Instance.OnDialogueEnded += DialogueEnded;
+    }
+
+    private void InitializeAnimations()
+    {
+        var idle = new AnimationState("Armature|idle");
+        var idle_dialogue = new AnimationState("Armature|idle_dialogue");
+
+        Animation.CreateAnimation(idle.Animation, true);
+        Animation.CreateAnimation(idle_dialogue.Animation, true);
+
+        Animation.Connect(idle, idle_dialogue, param_dialogue.WhenTrue());
+        Animation.Connect(idle_dialogue, idle, param_dialogue.WhenFalse());
+
+        Animation.Start(idle.Node);
     }
 
     public void Interact()
     {
-        var data = HandIn.GetOrCreateData(HandInInfo.Id);
-        HandInView.Instance.ShowPopup(data);
+        if (HandIn.IsHandInAvailable(HandInInfo.Id))
+        {
+            DialogueController.Instance.StartDialogue("##MUSHROOM_SWAMP_REQUEST_001##");
+        }
+        else
+        {
+            DialogueController.Instance.StartDialogue("##MUSHROOM_SWAMP_REQUEST_IDLE_001##");
+        }
+
+        param_dialogue.Set(true);
     }
 
     private void HandInClaimed(string id)
@@ -23,6 +54,21 @@ public partial class MushroomNpc : Area3D, IInteractable
         if (id == HandInInfo.Id)
         {
             HandIn.ResetData(HandInInfo);
+            DialogueController.Instance.StartDialogue("##MUSHROOM_SWAMP_REQUEST_COMPLETE_001##");
         }
+    }
+
+    private void DialogueEnded(string id)
+    {
+        if (id == "##MUSHROOM_SWAMP_REQUEST_002##")
+        {
+            var data = HandIn.GetOrCreateData(HandInInfo.Id);
+            HandInView.Instance.ShowPopup(data);
+        }
+    }
+
+    private void DialogueEnded()
+    {
+        param_dialogue.Set(false);
     }
 }
