66b9bf422da879b196c1c424f36d6a0d22bb5782
flawlesshappiness <bkk@arabesque.dk>
2025-07-18 23:09:46 +0200

    Game now starts with hat default color selected

--- froggyfocus/Character/FrogAppearanceData.cs
+++ froggyfocus/Character/FrogAppearanceData.cs
@@ -1,8 +1,8 @@
 public class FrogAppearanceData
 {
     public AppearanceColorType BodyColor { get; set; }
-    public bool HatPrimaryColorDefault { get; set; }
-    public bool HatSecondaryColorDefault { get; set; }
+    public bool HatPrimaryColorDefault { get; set; } = true;
+    public bool HatSecondaryColorDefault { get; set; } = true;
     public AppearanceColorType HatPrimaryColor { get; set; }
     public AppearanceColorType HatSecondaryColor { get; set; }
     public AppearanceHatType Hat { get; set; }
