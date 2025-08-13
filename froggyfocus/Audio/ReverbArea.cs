b1a02f5b202bf2b0f286b6899082d6c6b079e4ea
flawlesshappiness <bkk@arabesque.dk>
2025-08-13 20:52:46 +0200

    Added ReverbArea

---
+++ froggyfocus/Audio/ReverbArea.cs
@@ -0,0 +1,45 @@
+using Godot;
+using System.Collections;
+
+public partial class ReverbArea : Area3D
+{
+    [Export(PropertyHint.Range, "0,1,0.001")]
+    public float Amount;
+
+    private AudioBus bus;
+    private AudioEffectReverb reverb;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        BodyEntered += PlayerEntered;
+        BodyExited += PlayerExited;
+
+        bus = AudioBus.Get(AudioBusNames.SFX);
+        reverb = bus.GetEffect<AudioEffectReverb>();
+    }
+
+    private void PlayerEntered(GodotObject go)
+    {
+        FadeReverb(Amount, 1f);
+    }
+
+    private void PlayerExited(GodotObject go)
+    {
+        FadeReverb(0, 1f);
+    }
+
+    private Coroutine FadeReverb(float amount, float duration)
+    {
+        return this.StartCoroutine(Cr, "fade");
+        IEnumerator Cr()
+        {
+            var start = reverb.Wet;
+            var end = amount;
+            yield return LerpEnumerator.Lerp01(duration, f =>
+            {
+                reverb.Wet = Mathf.Lerp(start, end, f);
+            });
+        }
+    }
+}
