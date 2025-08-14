696b48cff364f50834cd59da07cfba09c6a02b22
flawlesshappiness <bkk@arabesque.dk>
2025-08-14 23:04:00 +0200

    OtterNpc now inherits CharacterNpc

--- froggyfocus/Prefabs/NPC/OtterNPC/OtterNpc.cs
+++ froggyfocus/Prefabs/NPC/OtterNPC/OtterNpc.cs
@@ -1,19 +1,9 @@
-using FlawLizArt.Animation.StateMachine;
 using Godot;
 
-public partial class OtterNpc : Area3D, IInteractable
+public partial class OtterNpc : CharacterNpc
 {
     [Export]
     public HandInInfo HandInInfo;
-
-    [Export]
-    public AnimationStateMachine Animation;
-
-    [Export]
-    public AudioStreamPlayer3D SfxSpeak;
-
-    private bool active_dialogue;
-    private readonly BoolParameter param_dialogue = new BoolParameter("dialogue", false);
 
     public override void _Ready()
     {
@@ -21,25 +11,10 @@ public partial class OtterNpc : Area3D, IInteractable
         HandIn.InitializeData(HandInInfo);
         HandInController.Instance.OnHandInClaimed += HandInClaimed;
 
-        DialogueController.Instance.OnNodeStarted += DialogueStarted;
-        DialogueController.Instance.OnNodeEnded += DialogueEnded;
-        DialogueController.Instance.OnDialogueEnded += DialogueEnded;
-
-        InitializeAnimations();
+        DialogueController.Instance.OnNodeEnded += DialogueNodeEnded;
     }
 
-    private void InitializeAnimations()
-    {
-        var idle = Animation.CreateAnimation("Armature|idle", true);
-        var look = Animation.CreateAnimation("Armature|look", true);
-
-        Animation.Connect(idle, look, param_dialogue.WhenTrue());
-        Animation.Connect(look, idle, param_dialogue.WhenFalse());
-
-        Animation.Start(idle.Node);
-    }
-
-    public void Interact()
+    public override void Interact()
     {
         if (HandIn.IsAvailable(HandInInfo.Id))
         {
@@ -62,33 +37,12 @@ public partial class OtterNpc : Area3D, IInteractable
         }
     }
 
-    private void StartDialogue(string id)
-    {
-        active_dialogue = true;
-        param_dialogue.Set(true);
-        DialogueController.Instance.StartDialogue(id);
-    }
-
-    private void DialogueStarted(string id)
-    {
-        if (active_dialogue)
-        {
-            SfxSpeak.Play();
-        }
-    }
-
-    private void DialogueEnded(string id)
+    private void DialogueNodeEnded(string id)
     {
         if (id == "##OTTER_SWAMP_REQUEST_002##")
         {
             var data = HandIn.GetOrCreateData(HandInInfo.Id);
             HandInView.Instance.ShowPopup(data);
         }
-    }
-
-    private void DialogueEnded()
-    {
-        param_dialogue.Set(false);
-        active_dialogue = false;
     }
 }
