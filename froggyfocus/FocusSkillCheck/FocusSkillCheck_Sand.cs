474f9900ccd08ed837238cfd68b77bcbcc9bc228
flawlesshappiness <bkk@arabesque.dk>
2025-08-18 01:57:10 +0200

    Added sand skill check

---
+++ froggyfocus/FocusSkillCheck/FocusSkillCheck_Sand.cs
@@ -0,0 +1,66 @@
+using Godot;
+using System.Collections;
+
+public partial class FocusSkillCheck_Sand : FocusSkillCheck
+{
+    [Export]
+    public Vector2 AmountRange;
+
+    [Export]
+    public Vector2 DurationRange;
+
+    [Export]
+    public GpuParticles3D PsWind;
+
+    [Export]
+    public AudioStreamPlayer SfxWind;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        SfxWind.Play();
+        SfxWind.VolumeLinear = 0;
+    }
+
+    public override void Clear()
+    {
+        base.Clear();
+        PsWind.Emitting = false;
+        SfxWind.VolumeLinear = 0;
+    }
+
+    protected override IEnumerator Run()
+    {
+        StartWind();
+        yield return null;
+    }
+
+    private Coroutine StartWind()
+    {
+        var angle = rng.RandfRange(0f, 360f);
+        var dir = Vector3.Forward.Rotated(Vector3.Up, Mathf.DegToRad(angle));
+        PsWind.GlobalRotationDegrees = new Vector3(0f, angle, 0f);
+
+        var wind_amount = AmountRange.Range(Difficulty);
+        var wind_duration = DurationRange.Range(Difficulty);
+        var time_end = GameTime.Time + wind_duration;
+
+        return this.StartCoroutine(Cr, "wind");
+        IEnumerator Cr()
+        {
+            PsWind.Emitting = true;
+
+            SfxWind.FadeIn(0.5f, 0);
+
+            while (GameTime.Time < time_end)
+            {
+                FocusEvent.Cursor.GlobalPosition += dir * wind_amount;
+                yield return null;
+            }
+
+            yield return SfxWind.FadeOut(0.5f);
+
+            Clear();
+        }
+    }
+}
