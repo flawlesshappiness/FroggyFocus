331e21b07ffbe7cb97eea4cf9740053608ff002d
flawlesshappiness <bkk@arabesque.dk>
2025-09-01 23:35:46 +0200

    Added factory door

--- froggyfocus/Prefabs/Objects/FactoryEntryDoor.cs
+++ froggyfocus/Prefabs/Objects/FactoryEntryDoor.cs
@@ -2,8 +2,12 @@ using Godot;
 
 public partial class FactoryEntryDoor : Area3D, IInteractable
 {
+    [Export]
+    public AudioStreamPlayer3D SfxLocked;
+
     public void Interact()
     {
+        SfxLocked.Play();
         DialogueController.Instance.StartDialogue("##LOCKED##");
     }
 }
