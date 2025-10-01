b210fd4be69220bc948603b31967f8f4f9500b51
flawlesshappiness <bkk@arabesque.dk>
2025-10-01 12:54:14 +0200

    added error label to freeze teleport

--- froggyfocus/FocusSkillCheck/FocusSkillCheck_FreezeTeleport.cs
+++ froggyfocus/FocusSkillCheck/FocusSkillCheck_FreezeTeleport.cs
@@ -6,9 +6,18 @@ public partial class FocusSkillCheck_FreezeTeleport : FocusSkillCheck
     [Export]
     public AudioStreamPlayer SfxFreeze;
 
+    [Export]
+    public Node3D ErrorLabel;
+
     public override void Clear()
     {
         base.Clear();
+    }
+
+    protected override void Stop()
+    {
+        base.Stop();
+        ErrorLabel.Hide();
     }
 
     protected override IEnumerator Run()
@@ -20,8 +29,12 @@ public partial class FocusSkillCheck_FreezeTeleport : FocusSkillCheck
         yield return new WaitForSecondsUnscaled(rng.RandfRange(0.2f, 0.5f));
         SfxFreeze.Stop();
 
+        ErrorLabel.GlobalPosition = Target.GlobalPosition;
+        ErrorLabel.Show();
+
         Target.GlobalPosition = Target.GetRandomPosition();
         Scene.PauseLock.SetLock(id, false);
+
         Clear();
     }
 }
