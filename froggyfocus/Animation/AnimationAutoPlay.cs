11ead9cad35cb53c857dbfb26c3bd7798daaeeda
flawlesshappiness <bkk@arabesque.dk>
2025-10-09 04:09:36 +0200

    added exhaust pipes to factory scene

--- froggyfocus/Animation/AnimationAutoPlay.cs
+++ froggyfocus/Animation/AnimationAutoPlay.cs
@@ -1,13 +1,24 @@
 using Godot;
+using System.Collections;
 
 public partial class AnimationAutoPlay : AnimationPlayer
 {
     [Export]
     public string Animation;
 
+    [Export]
+    public Vector2 Delay;
+
     public override void _Ready()
     {
         base._Ready();
-        Play(Animation);
+
+        this.StartCoroutine(Cr, "auto_animate");
+        IEnumerator Cr()
+        {
+            var rng = new RandomNumberGenerator();
+            yield return new WaitForSeconds(Delay.Range(rng.Randf()));
+            Play(Animation);
+        }
     }
 }
