466808ad3b192fb831f947250a475eae013ddc5b
flawlesshappiness <bkk@arabesque.dk>
2025-10-10 12:06:54 +0200

    wip eldritch scene

--- froggyfocus/Prefabs/Eldritch/EldritchEntrance.cs
+++ froggyfocus/Prefabs/Eldritch/EldritchEntrance.cs
@@ -10,11 +10,25 @@ public partial class EldritchEntrance : Area3D
 
     private bool is_active;
 
+    public override void _Ready()
+    {
+        base._Ready();
+
+        BodyEntered += PlayerEntered;
+    }
+
     public void Activate()
     {
         if (is_active) return;
+        is_active = true;
 
         Collider.Disabled = true;
         AnimationPlayer.Play("show");
+    }
+
+    private void PlayerEntered(GodotObject go)
+    {
+        Data.Game.CurrentScene = nameof(EldritchScene);
+        Scene.Goto(Data.Game.CurrentScene);
     }
 }
