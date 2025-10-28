4dd6ecc5c163dac468199fc19d520e65dc5b78f2
flawlesshappiness <bkk@arabesque.dk>
2025-10-28 07:17:04 +0100

    added eldritch flower to swampscene

---
+++ froggyfocus/Prefabs/Eldritch/EldritchFlower.cs
@@ -0,0 +1,93 @@
+using FlawLizArt.Animation.StateMachine;
+using Godot;
+using System;
+
+public partial class EldritchFlower : Area3D, IInteractable
+{
+    [Export]
+    public HandInInfo HandInInfo;
+
+    [Export]
+    public AnimationStateMachine Animation;
+
+    [Export]
+    public CollisionShape3D Collider;
+
+    public bool IsCompleted => HandIn.GetOrCreateData(HandInInfo.Id).ClaimedCount > 0;
+
+    private BoolParameter param_open = new BoolParameter("open", false);
+
+    public Action OnCompleted;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        InitializeHandIn();
+        InitializeAnimations();
+
+        HandInController.Instance.OnHandInClaimed += HandInClaimed;
+        DialogueController.Instance.OnNodeEnded += DialogueNodeEnded;
+    }
+
+    private void InitializeHandIn()
+    {
+        HandIn.InitializeData(HandInInfo);
+
+        if (IsCompleted)
+        {
+            DisableInteractive();
+        }
+    }
+
+    private void InitializeAnimations()
+    {
+        var idle_open = Animation.CreateAnimation("Armature|idle_open", true);
+        var idle_closed = Animation.CreateAnimation("Armature|idle_closed", true);
+        var open_to_closed = Animation.CreateAnimation("Armature|open_to_closed", false);
+        var closed_to_open = Animation.CreateAnimation("Armature|closed_to_open", false);
+
+        Animation.Connect(idle_open, open_to_closed, param_open.WhenFalse());
+        Animation.Connect(idle_closed, closed_to_open, param_open.WhenTrue());
+        Animation.Connect(open_to_closed, idle_closed);
+        Animation.Connect(closed_to_open, idle_open);
+
+        param_open.Set(!IsCompleted);
+        var start = IsCompleted ? idle_closed : idle_open;
+        Animation.Start(start.Node);
+    }
+
+    public void Interact()
+    {
+        if (IsCompleted)
+        {
+
+        }
+        else
+        {
+            DialogueController.Instance.StartDialogue("##ELDRITCH_FLOWER_HUNGRY##");
+        }
+    }
+
+    private void HandInClaimed(string id)
+    {
+        if (id != HandInInfo.Id) return;
+
+        DisableInteractive();
+        OnCompleted?.Invoke();
+    }
+
+    private void DisableInteractive()
+    {
+        Collider.Disabled = true;
+        param_open.Set(false);
+    }
+
+    private void DialogueNodeEnded(string id)
+    {
+        if (id == "##ELDRITCH_FLOWER_HUNGRY##")
+        {
+            var data = HandIn.GetOrCreateData(HandInInfo.Id);
+            HandInView.Instance.ShowPopup(data);
+        }
+    }
+}
