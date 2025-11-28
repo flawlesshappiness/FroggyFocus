d6127f8c85ef66ec03eaf74fc61071189d4a7967
flawlesshappiness <bkk@arabesque.dk>
2025-11-28 06:35:44 +0100

    Added dig skill check

---
+++ froggyfocus/FocusSkillCheck/FocusSkillCheck_Dig.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System.Collections;
+
+public partial class FocusSkillCheck_Dig : FocusSkillCheck
+{
+    [Export]
+    public Node3D TargetNode;
+
+    [Export]
+    public Node3D TargetParent;
+
+    [Export]
+    public AnimationPlayer AnimationPlayer;
+
+    public override void Clear()
+    {
+        base.Clear();
+        Target.SetParent(FocusEvent);
+    }
+
+    protected override IEnumerator Run()
+    {
+        TargetNode.GlobalPosition = Target.GlobalPosition;
+        TargetNode.GlobalRotation = Target.Character.GlobalRotation;
+
+        Target.SetParent(TargetParent);
+
+        yield return AnimationPlayer.PlayAndWaitForAnimation("dig_down");
+
+        var position = Target.GetRandomPosition();
+        TargetNode.GlobalPosition = position;
+
+        yield return AnimationPlayer.PlayAndWaitForAnimation("dig_up");
+
+        Clear();
+    }
+}
