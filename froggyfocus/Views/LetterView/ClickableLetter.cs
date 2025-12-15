8f9491022eea35901eb96f9749b778fe996ad95b
flawlesshappiness <bkk@arabesque.dk>
2025-12-15 07:19:32 +0100

    added more sounds to LetterScene

--- froggyfocus/Views/LetterView/ClickableLetter.cs
+++ froggyfocus/Views/LetterView/ClickableLetter.cs
@@ -5,9 +5,26 @@ using System.Collections;
 public partial class ClickableLetter : VBoxContainer
 {
     [Export]
+    public ClickableLetterArrow Arrow;
+
+    [Export]
+    public AudioStreamPlayer SfxText;
+
+    [Export]
     public Array<Label> Labels;
 
     private int idx_label = 0;
+    private bool waiting_for_input;
+
+    public override void _Input(InputEvent @event)
+    {
+        base._Input(@event);
+
+        if (waiting_for_input && PlayerInput.Interact.Pressed)
+        {
+            waiting_for_input = false;
+        }
+    }
 
     public void HideLabels()
     {
@@ -15,7 +32,10 @@ public partial class ClickableLetter : VBoxContainer
         foreach (var label in Labels)
         {
             label.VisibleCharacters = 0;
+            label.Hide();
         }
+
+        Arrow.Hide();
     }
 
     public Coroutine AnimateLabels()
@@ -25,11 +45,14 @@ public partial class ClickableLetter : VBoxContainer
         {
             while (idx_label < Labels.Count)
             {
+                Arrow.Hide();
                 yield return AnimateLabel();
                 idx_label++;
-                yield return null;
-                yield return new WaitForSeconds(0.5f);
-                // wait for input
+
+                Arrow.Show();
+                waiting_for_input = true;
+                while (waiting_for_input)
+                    yield return null;
             }
         }
     }
@@ -37,6 +60,9 @@ public partial class ClickableLetter : VBoxContainer
     public Coroutine AnimateLabel()
     {
         var label = Labels[idx_label];
+        label.Show();
+
+        SfxText.Play();
 
         return this.StartCoroutine(Cr, "show_label");
         IEnumerator Cr()
