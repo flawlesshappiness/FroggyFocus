4db938eef8303fdd2f2d220a48337474087f9328
flawlesshappiness <bkk@arabesque.dk>
2026-01-11 06:47:37 +0100

    Updated bestiary with variation buttons that display variation info

--- froggyfocus/Prefabs/UI/Bestiary/BestiaryEntryControl.cs
+++ froggyfocus/Prefabs/UI/Bestiary/BestiaryEntryControl.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 public partial class BestiaryEntryControl : Control
@@ -17,9 +18,6 @@ public partial class BestiaryEntryControl : Control
     public Control VariationContainer;
 
     [Export]
-    public Label VariationLabel;
-
-    [Export]
     public DifficultyStarsTexture DifficultyStars;
 
     [Export]
@@ -31,7 +29,12 @@ public partial class BestiaryEntryControl : Control
     [Export]
     public Button BackButton;
 
+    [Export]
+    public InventoryPreviewButton VariationPreviewButton;
+
     public event Action BackPressed;
+
+    private List<InventoryPreviewButton> variation_buttons = new();
 
     public override void _Ready()
     {
@@ -46,28 +49,71 @@ public partial class BestiaryEntryControl : Control
 
     public void Load(FocusCharacterInfo info)
     {
-        var stats = StatsController.Instance.GetOrCreateCharacterData(info.ResourcePath);
+        UpdateText(info);
+        UpdateBigPreview(info);
 
+        var stats = StatsController.Instance.GetOrCreateCharacterData(info.ResourcePath);
+        DifficultyStars.SetStars(stats.HighestRarity);
+
+        CreateVariationButtons(info);
+
+        var variations = FocusCharacterController.Instance.Collection.Resources.Where(x => x != info && x.Variation == info.Variation).ToList();
+        VariationContainer.Visible = variations.Count > 0;
+    }
+
+    private void UpdateText(FocusCharacterInfo info)
+    {
+        var stats = StatsController.Instance.GetOrCreateCharacterData(info.ResourcePath);
         NameLabel.Text = info.Name;
         LocationLabel.Text = Tr(info.LocationHint);
         CaughtLabel.Text = $"{stats.CountCaught}";
-        DifficultyStars.SetStars(stats.HighestRarity);
+    }
 
-        var variations = FocusCharacterController.Instance.Collection.Resources.Where(x => x != info && x.Variation == info.Variation).ToList();
-        var variation_text = string.Empty;
-        for (int i = 0; i < variations.Count; i++)
-        {
-            var v = variations[i];
-            var v_stats = StatsController.Instance.GetOrCreateCharacterData(v.ResourcePath);
-            var caught = v_stats.CountCaught > 0;
-            Debug.Log($"{v.Name} {v_stats.CountCaught}");
-            variation_text += i > 0 ? "\n" : string.Empty;
-            variation_text += caught ? $"{v.Name}" : "???";
-        }
-        VariationLabel.Text = variation_text;
-        VariationContainer.Visible = variations.Count > 0;
-
+    private void UpdateBigPreview(FocusCharacterInfo info)
+    {
         ItemSubViewport.SetCharacter(info);
+    }
+
+    private void CreateVariationButtons(FocusCharacterInfo info)
+    {
+        VariationPreviewButton.Hide();
+
+        // Clear
+        foreach (var button in variation_buttons)
+        {
+            button.QueueFree();
+        }
+        variation_buttons.Clear();
+
+        // Create buttons
+        var variations = FocusCharacterController.Instance.Collection.Resources.Where(x => x != info && x.Variation == info.Variation).ToList();
+        CreateVariationButton(info);
+
+        foreach (var v_info in variations)
+        {
+            CreateVariationButton(v_info);
+        }
+    }
+
+    private void CreateVariationButton(FocusCharacterInfo info)
+    {
+        var stats = StatsController.Instance.GetOrCreateCharacterData(info.ResourcePath);
+
+        var button = VariationPreviewButton.Duplicate() as InventoryPreviewButton;
+        button.SetParent(VariationPreviewButton.GetParent());
+        button.Show();
+        button.SetCharacter(info);
+        button.SetObscured(stats.CountCaught == 0);
+        variation_buttons.Add(button);
+
+        if (stats.CountCaught > 0)
+        {
+            button.Pressed += () =>
+            {
+                UpdateText(info);
+                UpdateBigPreview(info);
+            };
+        }
     }
 
     private void BackButton_Pressed()
