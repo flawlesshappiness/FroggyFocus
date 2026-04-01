1c38ddc41e751434abb84f1fa856de08a7afdf03
flawlesshappiness <bkk@arabesque.dk>
2026-04-01 15:47:19 +0200

    Added dramatic zoom when catching bug

--- froggyfocus/FocusEvent/FocusEvent.cs
+++ froggyfocus/FocusEvent/FocusEvent.cs
@@ -27,6 +27,9 @@ public partial class FocusEvent : Node3D
     public Marker3D CameraIntroMarker;
 
     [Export]
+    public Marker3D MainCameraMarker;
+
+    [Export]
     public PackedScene FocusTargetPrefab;
 
     [Export]
@@ -70,6 +73,7 @@ public partial class FocusEvent : Node3D
     public Settings CurrentSettings { get; private set; }
     public List<FocusTarget> Targets { get; private set; } = new();
     public bool IsCoveringEyes { get; private set; }
+    public bool IsEating { get; private set; }
 
     private List<FocusSkillCheck> skill_checks = new();
     private List<FocusEventBackground> backgrounds = new();
@@ -143,6 +147,7 @@ public partial class FocusEvent : Node3D
 
         Process_Timer();
         Process_Facing();
+        Process_MainCamera();
     }
 
     public override void _Input(InputEvent @event)
@@ -180,6 +185,25 @@ public partial class FocusEvent : Node3D
         if (!Cursor.HasTarget) return;
 
         Frog.StartFacingPosition(Cursor.CurrentTarget.GlobalPosition);
+    }
+
+    private void Process_MainCamera()
+    {
+        var speed = 1f;
+        if (Cursor.HasTarget)
+        {
+            var start = MainCameraMarker.GlobalPosition;
+            var target = Cursor.CurrentTarget.GlobalPosition;
+            var middle = start.Lerp(target, 0.3f).Set(y: start.Y);
+            var t = Cursor.CurrentTarget.FocusValue / Cursor.CurrentTarget.FocusMax;
+            var y = Mathf.Lerp(start.Y, target.Y, 0.25f);
+            var zoom = middle.Set(y: Mathf.Lerp(start.Y, y, t));
+            Camera.GlobalPosition = Camera.GlobalPosition.Lerp(zoom, speed * GameTime.DeltaTime);
+        }
+        else if (!IsEating)
+        {
+            Camera.GlobalPosition = Camera.GlobalPosition.Lerp(MainCameraMarker.GlobalPosition, speed * GameTime.DeltaTime);
+        }
     }
 
     private FocusTarget CreateTarget()
@@ -419,6 +443,8 @@ public partial class FocusEvent : Node3D
         this.StartCoroutine(Cr, "catch");
         IEnumerator Cr()
         {
+            IsEating = true;
+
             FocusCursor.MoveLock.SetLock("catch", true);
             Cursor.Hide();
 
@@ -436,6 +462,8 @@ public partial class FocusEvent : Node3D
             FocusCursor.MoveLock.SetLock("catch", false);
 
             target.Caught();
+
+            IsEating = false;
         }
     }
 
@@ -465,231 +493,4 @@ public partial class FocusEvent : Node3D
             GameView.Instance.AnimateVignetteHide(0.5f);
         }
     }
-
-    /*
-    public virtual void StartEvent_OLD()
-    {
-        // Disable player
-        Player.SetAllLocks(nameof(FocusEvent), true);
-
-        this.StartCoroutine(Cr, "event");
-        IEnumerator Cr()
-        {
-            // Target
-            CreateTarget();
-
-            // Frog
-            Frog.StartFacingPosition(GlobalPosition);
-
-            // Clear skill checks
-            ResetSkillchecks();
-
-            // Transition start
-            FocusIntroView.Instance.LoadTarget(Target);
-            yield return FocusIntroView.Instance.AnimateShow();
-
-            // Show event
-            Show();
-
-            // Hijack camera
-            HijackCamera();
-
-            // Game View
-            GameView.Instance.SetFocusEventControlsVisible(true);
-
-            // Transition end
-            yield return WaitForRiff();
-            yield return FocusIntroView.Instance.AnimateHide();
-
-            // Initialize cursor
-            Cursor.GlobalPosition = Target.GlobalPosition;
-            Cursor.Start(Target);
-
-            // Start
-            EventStarted = true;
-            FocusEventController.Instance.FocusEventStarted(this);
-            this.StartCoroutine(EventCr, "event")
-                .SetRunWhilePaused(true);
-        }
-    }
-    */
-    /*
-    protected virtual void EndEvent(bool completed)
-    {
-        OnStopped?.Invoke();
-
-        this.StartCoroutine(Cr, "event");
-        IEnumerator Cr()
-        {
-            // Disable cursor
-            Cursor.Stop();
-
-            if (completed)
-            {
-                yield return WaitForCatchTarget();
-            }
-            else
-            {
-                yield return WaitForLoseTarget();
-            }
-
-            // Transition
-            yield return WaitForTransition();
-
-            // Camera target player
-            Player.Instance.SetCameraTarget();
-
-            // Hide target
-            Target.Hide();
-
-            // GameView
-            GameView.Instance.SetFocusEventControlsVisible(false);
-
-            // Enable player
-            Player.SetAllLocks(nameof(FocusEvent), false);
-
-            // End
-            EventStarted = false;
-
-            if (completed)
-            {
-                var result = new FocusEventCompletedResult(this);
-                OnCompleted?.Invoke(result);
-                FocusEventController.Instance.FocusEventCompleted(result);
-            }
-            else
-            {
-                var result = new FocusEventFailedResult(this);
-                OnFailed?.Invoke(result);
-                FocusEventController.Instance.FocusEventFailed(result);
-            }
-
-            // Save
-            Data.Game.Save();
-        }
-    }
-
-    private void EndEventPrematurely()
-    {
-        if (!EventStarted) return;
-
-        Cursor.AdjustFocusValue(-99999);
-    }
-
-    private IEnumerator EventCr()
-    {
-        var rng = new RandomNumberGenerator();
-
-        while (true)
-        {
-            // Move target
-            if (!Target.Info.IsStationary)
-            {
-                yield return Target.WaitForMoveToRandomPosition();
-            }
-
-            // Wait
-            var duration = Target.GetMoveDelay();
-            if (duration > 0)
-            {
-                Target.StopMoving();
-                yield return new WaitForSeconds(duration);
-            }
-
-            // Skill check
-            var has_override_skill_check = OverrideSkillCheck != null;
-            var has_skill_checks = Target.Info.SkillChecks?.Count > 0 || has_override_skill_check;
-            var is_skill_check = true || rng.Randf() < 0.5f;
-            if (has_skill_checks && is_skill_check)
-            {
-                Target.StopMoving();
-                yield return WaitForSkillCheck();
-            }
-        }
-    }
-
-    private IEnumerator WaitForRiff()
-    {
-        if (Data.Options.CutsceneTypeIndex == 1) // Fast
-        {
-            yield return new WaitForSeconds(0.25f);
-        }
-        else if (!MusicController.Instance.IsMusicPlaying())
-        {
-            MusicController.Instance.MuteLock.AddLock(nameof(FocusIntroView));
-            FocusIntroView.Instance.PlayRiff();
-            yield return new WaitForSeconds(1.0f);
-            MusicController.Instance.MuteLock.RemoveLock(nameof(FocusIntroView));
-        }
-        else
-        {
-            yield return new WaitForSeconds(0.5f);
-        }
-    }
-
-    private IEnumerator WaitForSkillCheck()
-    {
-        var type =
-            (OverrideSkillCheck != null && OverrideSkillCheck.IsAvailable()) ? OverrideSkillCheck.Type
-            : Target.Info.SkillChecks.Count == 0 ? FocusSkillCheckType.Dash
-            : Target.Info.SkillChecks?.Where(x => skill_checks.FirstOrDefault(y => y.Type == x)?.IsAvailable() ?? true).ToList().Random();
-        var skill_check = skill_checks.FirstOrDefault(x => x.Type == type);
-
-        if (skill_check == null)
-        {
-            yield return null;
-        }
-        else
-        {
-            yield return skill_check.Start();
-        }
-    }
-
-    private void FocusFilled()
-    {
-        Data.Game.TargetsCollected++;
-        Data.Game.Save();
-
-        EndEvent(true);
-    }
-
-    private void FocusEmpty()
-    {
-        EndEvent(false);
-    }
-
-    private void FocusTarget()
-    {
-        Frog.SetHandToPosition(Target.GlobalPosition);
-    }
-
-    private IEnumerator WaitForCatchTarget()
-    {
-        Frog.SetHandsBack();
-        Frog.StartFacingPosition(Target.GlobalPosition);
-        Target.Animate_Scared();
-        yield return WaitForSuspense();
-        Target.Animate_Unscared();
-        SfxSuspenseNormal_Success.Play();
-        GameView.Instance.AnimateVignetteHide(0.25f);
-        AnimateZoomOut(0.25f);
-        yield return Frog.AnimateEatTarget(Target.Character);
-        yield return FocusOutroView.Instance.WaitForInventory(Target);
-        yield return new WaitForSeconds(0.25f);
-    }
-
-    private IEnumerator WaitForLoseTarget()
-    {
-        Frog.SetHandsBack();
-        yield return WaitForSuspense();
-        SfxSuspenseNormal_Fail.Play();
-        GameView.Instance.AnimateVignetteHide(0.25f);
-        AnimateZoomOut(0.25f);
-        AnimationPlayer_Frog.Play("scared");
-        Frog.SetMouthOpen(true);
-        yield return Target.Animate_Disappear();
-        yield return new WaitForSeconds(0.25f);
-        Frog.SetMouthOpen(false);
-    }
-    */
 }
