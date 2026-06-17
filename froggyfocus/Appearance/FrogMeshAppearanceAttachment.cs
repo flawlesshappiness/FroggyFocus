31e62b37a34c05a0c4f18bd8a9aec42690bb7d5f
flawlesshappiness <bkk@arabesque.dk>
2026-06-17 13:47:21 +0200

    fixed frog mesh previews

---
+++ froggyfocus/Appearance/FrogMeshAppearanceAttachment.cs
@@ -0,0 +1,83 @@
+using Godot;
+using Godot.Collections;
+using System.Collections.Generic;
+
+public partial class FrogMeshAppearanceAttachment : AppearanceAttachment
+{
+    [Export]
+    public Array<MeshInstance3D> OnlySecondaryMeshes;
+
+    private List<MeshGroup> meshes = new();
+
+    private class MeshGroup
+    {
+        public MeshInstance3D Mesh { get; set; }
+        public ShaderMaterial PrimaryMaterial { get; set; }
+        public ShaderMaterial SecondaryMaterial { get; set; }
+    }
+
+    public override void _Ready()
+    {
+        base._Ready();
+        InitializeMeshes();
+    }
+
+    private void InitializeMeshes()
+    {
+        try
+        {
+            var ms = this.GetNodesInChildren<MeshInstance3D>();
+            foreach (var mesh in ms)
+            {
+                var group = new MeshGroup();
+                meshes.Add(group);
+
+                var count = mesh.GetSurfaceOverrideMaterialCount();
+                var is_only_secondary = OnlySecondaryMeshes?.Contains(mesh) ?? false;
+
+                if (!is_only_secondary)
+                {
+                    group.PrimaryMaterial = mesh.GetActiveMaterial(0).Duplicate() as ShaderMaterial;
+                    mesh.SetSurfaceOverrideMaterial(0, group.PrimaryMaterial);
+                }
+                else
+                {
+                    group.SecondaryMaterial = mesh.GetActiveMaterial(0).Duplicate() as ShaderMaterial;
+                    mesh.SetSurfaceOverrideMaterial(0, group.SecondaryMaterial);
+                }
+
+                if (count > 1)
+                {
+                    group.SecondaryMaterial = mesh.GetActiveMaterial(1).Duplicate() as ShaderMaterial;
+                    mesh.SetSurfaceOverrideMaterial(1, group.SecondaryMaterial);
+                }
+            }
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError($"Failed to initialize mesh on {Name}");
+        }
+    }
+
+    public override void SetPrimaryColor(Color color)
+    {
+        foreach (var mesh in meshes)
+        {
+            if (mesh == null) continue;
+            if (mesh.PrimaryMaterial == null) continue;
+
+            //mesh.PrimaryMaterial.SetShaderParameter("albedo1", color);
+        }
+    }
+
+    public override void SetSecondaryColor(Color color)
+    {
+        foreach (var mesh in meshes)
+        {
+            if (mesh == null) continue;
+            if (mesh.SecondaryMaterial == null) continue;
+
+            //mesh.PrimaryMaterial.SetShaderParameter("albedo2", color);
+        }
+    }
+}
