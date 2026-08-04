5a92e7cd888754d7c53b88a628f76a82a55f8560
flawlesshappiness <bkk@arabesque.dk>
2026-08-04 18:05:30 +0200

    added focus lock to Flower attack

--- froggyfocus/FocusAttack/Flower.cs
+++ froggyfocus/FocusAttack/Flower.cs
@@ -7,11 +7,20 @@ public partial class Flower : TrapAttack<CursorFlower>
     protected override IEnumerator AnimateCharacterTrap()
     {
         Target.Animate_Exclamation();
+
+        SetLock(true);
         yield return Target.Animate_DiveDown();
 
         var position = Target.GetNextPosition();
         Target.GlobalPosition = position;
 
+        SetLock(false);
         yield return Target.Animate_DiveUp();
+    }
+
+    private void SetLock(bool locked)
+    {
+        var id = nameof(Dive);
+        Target.FocusLock.SetLock(id, locked);
     }
 }
